47a9f30c60c7c088dd64263867e1885c73bc3d6b
Cloud-Gen-DevOps-Projects <ravindra.devops@gmail.com>
2024-06-25 08:13:19 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-03f4878755434977f"
+  ami                    = "ami-0f58b397bc5c1f2e8"
   instance_type          = "t2.medium"
-  key_name               = "mumbai"
+  key_name               = "AWS-Zomato-Project"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
